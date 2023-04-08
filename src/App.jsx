231b55fddb6be7bfdd16ab48842dab7d4e8a15fe
gauti123456 <geekygautam1997@gmail.com>
2023-04-08 14:36:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Users from './Users'
+import { UserProvider } from './provider/UserProvider'
+
+function App() {
+
+  return (
+    <UserProvider>
+     <Users/>
+    </UserProvider>
+  )
+}
+
+export default App
